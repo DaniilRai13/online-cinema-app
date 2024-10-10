6d04661aa1a9a456018a34d8898115bcb1291293
DaniilRai13 <testnet02@mail.ru>
2024-10-10 13:28:31 +0300

    init: set the initial project structure

---
+++ app/components/screens/Home/Home.interface.ts
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react'
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface IHome { }
+export interface ILayout {
+	children?: ReactNode
+}

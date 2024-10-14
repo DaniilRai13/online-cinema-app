fe72736e7b86b5cd6402a05faa4a24c8bddb34cf
DaniilRai13 <testnet02@mail.ru>
2024-10-14 16:24:27 +0300

    feat: add Gener menu component

--- app/components/layout/Navigation/MenuContainer/MenuContainer.tsx
+++ app/components/layout/Navigation/MenuContainer/MenuContainer.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react'
 import Menu from './Menu/Menu'
+import GenreMenu from './genres/GenreMenu'
 import { firstMenu, userMenu } from './menu.data'
 
 const MenuContainer: FC = () => {
 	return (
 		<div>
 			<Menu menu={firstMenu} />
+			<GenreMenu />
 			<Menu menu={userMenu} />
 		</div>
 	)

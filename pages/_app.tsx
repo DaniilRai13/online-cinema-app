f2caad07ee06b554b3be3e7b48316718ad300709
DaniilRai13 <testnet02@mail.ru>
2024-10-15 17:29:39 +0300

    refactor: change the core .next files in project

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from 'next/app'
+import MainProvider from 'providers/MainProvider'
+import '../app/globals.scss'
+
+export default function App({ Component, pageProps }: AppProps) {
+	return (
+		<MainProvider>
+			<Component {...pageProps} />
+		</MainProvider>
+	)
+}

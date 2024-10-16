9551dab5fd9fcefd3b991471385408d8b1518a6d
DaniilRai13 <testnet02@mail.ru>
2024-10-16 19:59:00 +0300

    feat: get movies form database

---
+++ app/services/movie.service.ts
@@ -0,0 +1,15 @@
+import { getMoviesUrl } from '@/config/api.config'
+import { IMovie } from '@/shared/types/movie.types'
+import { axiosClassic } from 'api/interceptors'
+
+export const MovieService = {
+	async getAll(searchTerm?: string) {
+		return axiosClassic.get<IMovie[]>(getMoviesUrl(''), {
+			params: searchTerm
+				? {
+					searchTerm
+				}
+				: {}
+		})
+	}
+}

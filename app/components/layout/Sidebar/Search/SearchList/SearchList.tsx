5240d1c40adc200ce0a941d3588bd8a04efddb34
DaniilRai13 <testnet02@mail.ru>
2024-10-16 20:05:22 +0300

    feat: creates a block that displays a list of movies

---
+++ app/components/layout/Sidebar/Search/SearchList/SearchList.tsx
@@ -0,0 +1,43 @@
+import { getMovieUrl } from '@/config/url.config'
+import { IMovie } from '@/shared/types/movie.types'
+import { FC } from 'react'
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+import style from './SearchList.module.scss'
+interface LoaderProp {
+	src: string
+}
+const SearchList: FC<{ movies: IMovie[] }> = ({ movies }) => {
+	const loaderProp = ({ src }: LoaderProp): string => {
+		return src
+	}
+	console.log(movies)
+	return (
+		<div className={style.list}>
+			{movies.length
+				?
+				movies.map((movie) => (
+					<Link key={movie._id} href={getMovieUrl(movie.slug)}>
+						<a>
+							<Image
+								src={movie.poster}
+								width={50}
+								height={50}
+								objectFit='cover'
+								objectPosition='top'
+								draggable={false}
+								alt={movie.title}
+								loader={loaderProp}
+							/>
+						</a>
+						<span>{movie.title}</span>
+					</Link>)
+				)
+				: (<div className='text-white text-center my-4'>Movies not found</div>)}
+		</div>
+	)
+}
+
+export default SearchList

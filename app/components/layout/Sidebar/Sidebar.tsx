4cbb025468643404b867e03044fadaac9f129a13
DaniilRai13 <testnet02@mail.ru>
2024-10-17 16:03:54 +0300

    feat: add container of a popular movies

--- app/components/layout/Sidebar/Sidebar.tsx
+++ app/components/layout/Sidebar/Sidebar.tsx
@@ -1,10 +1,14 @@
 import React, { FC } from 'react'
 import Search from './Search/Search'
 import styles from './Sidebar.module.scss'
+import MoviesContainer from './MoviesContainer/MoviesContainer'
 const Sidebar: FC = () => {
 	return (
-		<div className={styles.sidebar}><Search /></div>
-		
+		<div className={styles.sidebar}>
+			<Search />
+			<MoviesContainer />
+		</div>
+
 	)
 }
 

d09837f6892fae78b29d08fddcabfe06f64b3a2b
DaniilRai13 <testnet02@mail.ru>
2024-10-28 22:37:32 +0300

    refactor: move the toastr reducer to the end of the list of reducers, as recommended by the toastr documentation

--- app/store/rootReducers.ts
+++ app/store/rootReducers.ts
@@ -1,7 +1,7 @@
 import { reducer as toastrReducer } from 'react-redux-toastr'
-import {userReducer} from './user/userSlice'
+import { userReducer } from './user/userSlice'
 
 export const reducers = {
+	user: userReducer,
 	toastr: toastrReducer,
-	user: userReducer
 }

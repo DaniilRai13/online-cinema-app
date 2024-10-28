d7c72e5722d71a70ad6caa712adc31f8a9e30c81
DaniilRai13 <testnet02@mail.ru>
2024-10-28 15:04:20 +0300

    feat: add userReducer in store

--- app/store/rootReducers.ts
+++ app/store/rootReducers.ts
@@ -1,5 +1,7 @@
-import {reducer as toastrReducer} from 'react-redux-toastr'
+import { reducer as toastrReducer } from 'react-redux-toastr'
+import {userReducer} from './user/userSlice'
 
 export const reducers = {
-	toastr: toastrReducer
+	toastr: toastrReducer,
+	user: userReducer
 }

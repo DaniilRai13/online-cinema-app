a39950b48409533ba2e2f63bde63e9aa36fddbaf
DaniilRai13 <testnet02@mail.ru>
2024-10-29 17:51:28 +0300

    feat: add profile and logout options for logged-in users

--- app/components/layout/Navigation/MenuContainer/auth/AuthItems.tsx
+++ app/components/layout/Navigation/MenuContainer/auth/AuthItems.tsx
@@ -1,8 +1,40 @@
-import { FC } from 'react'
 
-const AuthItems:FC = () => {
+import { getAdminHomeUrl } from '@/config/url.config'
+import { useAuth } from '@/hooks/useAuth'
+import { FC } from 'react'
+import MenuItem from '../Menu/MenuItem/MenuItem'
+import LogoutButton from './LogoutButton'
+
+const AuthItems: FC = () => {
+	const { user } = useAuth()
+
 	return (
-		<div>AuthItems</div>
+		<>
+			{
+				user?.isAdmin && <MenuItem item={
+					{
+						icon: 'MdOutlineLock',
+						title: 'Admin Panel',
+						link: getAdminHomeUrl()
+					}
+				} />
+			}
+			{user ? <>
+				<MenuItem item={{
+					icon: 'MdSettings',
+					title: 'Profile',
+					link: '/profile'
+				}} />
+				<LogoutButton />
+			</>
+				: <MenuItem item={{
+					icon: 'MdLogin',
+					title: 'Login',
+					link: '/auth'
+				}} />}
+
+
+		</>
 	)
 }
 

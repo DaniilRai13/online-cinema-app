3c862257e83ac1780e862ef1dc48aea155729e17
DaniilRai13 <testnet02@mail.ru>
2024-10-29 17:51:55 +0300

    feat: create logout button

---
+++ app/components/layout/Navigation/MenuContainer/auth/LogoutButton.tsx
@@ -0,0 +1,23 @@
+import { MaterialIcon } from '@/components/ui/MaterialIcon'
+import { useActions } from '@/hooks/useActions'
+import { FC, MouseEvent } from 'react'
+
+const LogoutButton: FC = () => {
+	const { logout } = useActions()
+
+	const logoutHandler = (e: MouseEvent<HTMLAnchorElement>) => {
+		e.preventDefault()
+		logout()
+	}
+
+	return (
+		<li>
+			<a onClick={logoutHandler}>
+				<MaterialIcon name="MdLogout" />
+				<span>Logout</span>
+			</a>
+		</li>
+	)
+}
+
+export default LogoutButton

388c3dd34192349bcc4dccab09a14a7665826455
DaniilRai13 <testnet02@mail.ru>
2024-10-29 18:03:52 +0300

    feat: add actions handling

--- app/store/user/userSlice.ts
+++ app/store/user/userSlice.ts
@@ -1,5 +1,6 @@
 import { getStoreLocal } from '@/utils/local-storage'
 import { createSlice } from '@reduxjs/toolkit'
+import { checkAuth, login, logout, register } from './user.actions'
 
 const initialState = {
 	isLoading: false,
@@ -11,7 +12,44 @@ export const userSlice = createSlice({
 	initialState,
 	reducers: {},
 	extraReducers: (builder) => {
+		builder.addCase(register.pending, state => {
+			state.isLoading = true
+		})
+		builder.addCase(register.fulfilled, (state, { payload }) => {
+			state.isLoading = false
+			state.user = payload.user
+		})
+		builder.addCase(register.rejected, state => {
+			state.isLoading = false
+			state.user = null
+		})
 
+		//LOGIN
+
+		builder.addCase(login.pending, state => {
+			state.isLoading = true
+		})
+		builder.addCase(login.fulfilled, (state, { payload }) => {
+			state.isLoading = false
+			state.user = payload.user
+		})
+		builder.addCase(login.rejected, state => {
+			state.isLoading = false
+			state.user = null
+		})
+
+		//LOGOUT 
+		builder.addCase(logout.fulfilled, state => {
+			state.isLoading = false
+			state.user = null
+		})
+
+		//CHECK_AUTH
+
+		builder.addCase(checkAuth.fulfilled, (state, { payload }) => {
+			state.isLoading = false
+			state.user = payload.user
+		})
 	}
 })
 

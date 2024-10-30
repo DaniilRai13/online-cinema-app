7bf379d061ef76c3d93a6664f0855770656e3ef0
DaniilRai13 <testnet02@mail.ru>
2024-10-30 12:47:59 +0300

    feat: add interceptor to include authorization token in request headers

--- app/api/interceptors.ts
+++ app/api/interceptors.ts
@@ -1,9 +1,28 @@
 import { API_URL } from '@/config/api.config'
+// import { AuthService } from '@/services/auth/auth.service'
 import axios from 'axios'
+import Cookies from 'js-cookie'
+import { getContentType } from './api.helper'
+// import { removeTokensStorage } from '@/services/auth/auth.helper'
 
 export const axiosClassic = axios.create({
 	baseURL: API_URL,
-	headers: {
-		'Content-Type': 'application/json',
-	},
+	headers: getContentType()
 })
+
+const instance = axios.create({
+	baseURL: API_URL,
+	headers: getContentType()
+})
+
+instance.interceptors.request.use((config) => {
+	const accessToken = Cookies.get('accessToken')
+
+	if (config.headers && accessToken) {
+		config.headers.Authorization = `Bearer ${accessToken}`
+	}
+
+	return config
+})
+
+export default instance

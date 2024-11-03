ea932e629acc36f7d19487800dfd9f38cfe3f12f
DaniilRai13 <testnet02@mail.ru>
2024-11-03 16:20:05 +0300

    feat: implement AuthProvider for user authentication and role checking

---
+++ app/providers/AuthProvider/AuthProvider.tsx
@@ -0,0 +1,51 @@
+import { useActions } from '@/hooks/useActions'
+import { useAuth } from '@/hooks/useAuth'
+import { TypeComponentAuthFields } from '@/shared/types/auth.types'
+import Cookies from 'js-cookie'
+import dynamic from 'next/dynamic'
+import { useRouter } from 'next/navigation'
+import { FC, ReactNode, useEffect, useState } from 'react'
+
+const DynamicImportRole = dynamic(() => import('./CheckRole'), { ssr: false })
+
+const AuthProvider: FC<TypeComponentAuthFields & { children: ReactNode }> = ({ children, Component: { isOnlyUser, isOnlyAdmin } }) => {
+	const [isLoading, setIsLoading] = useState(true)
+
+	const { user } = useAuth()
+	const { logout, checkAuth } = useActions()
+	const router = useRouter()
+
+	const Children = () => {
+		return <>{children}</>
+	}
+
+	useEffect(() => {
+		const accessToken = Cookies.get('accessToken')
+
+		if (accessToken) {
+			checkAuth()
+			setIsLoading(false)
+		} else {
+			setIsLoading(false)
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+
+	useEffect(() => {
+		const refreshToken = Cookies.get('refreshToken')
+		if (!refreshToken && user) {
+			logout()
+			router.push('/auth')
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+
+
+	return isLoading ? <div> Loading...</div > : (!isOnlyAdmin && !isOnlyUser
+		? <Children />
+		: <DynamicImportRole Component={{ isOnlyAdmin, isOnlyUser }}>
+			{children}
+		</DynamicImportRole>)
+}
+
+export default AuthProvider

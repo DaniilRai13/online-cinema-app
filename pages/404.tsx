5fc268ef019560a787029886bc9767182ebb45c8
DaniilRai13 <testnet02@mail.ru>
2024-11-04 19:25:36 +0300

    refactor: redo component

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,13 @@
 import Heading from '@/components/ui/heading/Heading'
 import Meta from '@/utils/meta/Meta'
+import { NextPage } from 'next'
 
-export default function Error404() {
+const Error404: NextPage = () => {
 	return (
 		<Meta title='Page not found'>
 			<Heading title='404 - Page Not Found' />
 		</Meta>
 	)
 }
+
+export default Error404

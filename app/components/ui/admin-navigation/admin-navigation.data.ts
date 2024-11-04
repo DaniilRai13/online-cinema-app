4263c5cb3ff4d3f44370e12ba9fa335a7a8a47b4
DaniilRai13 <testnet02@mail.ru>
2024-11-04 19:28:59 +0300

    feat: add data about link in admin page navigation

---
+++ app/components/ui/admin-navigation/admin-navigation.data.ts
@@ -0,0 +1,21 @@
+import { getAdminHomeUrl, getAdminUrl } from '@/config/url.config'
+import { INavItem } from './admin-navigation.interface'
+
+export const navItems: INavItem[] = [
+	{
+		title: 'Statistics',
+		link: getAdminHomeUrl()
+	},
+	{
+		title: 'Users',
+		link: getAdminUrl('users')
+	},
+	{
+		title: 'Actors',
+		link: getAdminUrl('actors')
+	},
+	{
+		title: 'Genres',
+		link: getAdminUrl('genres')
+	}
+]

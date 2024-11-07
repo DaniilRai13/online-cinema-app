00ceacded62d45b288b6a0a061689cea237001f6
DaniilRai13 <testnet02@mail.ru>
2024-11-07 19:56:50 +0300

    refactor: change directory path

--- pages/manage/users.tsx
+++ pages/manage/users.tsx
@@ -1,4 +1,4 @@
-import UsersList from '@/components/screens/Admin/Home/Users/UsersList'
+import UsersList from '@/components/screens/Admin/Users/UsersList'
 import { NextPageAuth } from '@/shared/types/auth.types'
 
 const UsersListPage: NextPageAuth = () => {

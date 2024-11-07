64e55685b39a29bd035c558a6712c7331276ed5c
DaniilRai13 <testnet02@mail.ru>
2024-11-07 18:52:25 +0300

    refactor: change path

--- app/components/screens/Admin/Admin.tsx
+++ app/components/screens/Admin/Admin.tsx
@@ -1,7 +1,7 @@
 import Heading from '@/components/ui/heading/Heading'
 import Meta from '@/utils/meta/Meta'
 import { FC } from 'react'
-import Statistics from './Statistics/Statistics'
+import Statistics from './Home/Statistics/Statistics'
 import AdminNavigation from '@/components/ui/admin-navigation/AdminNavigation'
 
 const Admin: FC = () => {

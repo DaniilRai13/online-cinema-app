c782f5ac92877e2b430c847ea1156e2efc754540
DaniilRai13 <testnet02@mail.ru>
2024-11-10 19:52:55 +0300

    feat: create interface and function that make a string with genres name

--- app/utils/movie/getGenresListEach.ts
+++ app/utils/movie/getGenresListEach.ts
@@ -1,3 +1,7 @@
 export const getGenresListEach = (index: number, name: string, length: number) => {
 	return index + 1 === length ? name : `${name}, `
 }
+interface IArrayItem {
+	name: string
+}
+export const getGenresList = (array: IArrayItem[]) => array.map(item => item.name).join(',')

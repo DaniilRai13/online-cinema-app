979bb67d283274395822c3a418fce304f0b00021
DaniilRai13 <testnet02@mail.ru>
2024-11-10 21:25:26 +0300

    feat: integrated `react-query` for data fetching (`getById`) and mutation (`update`).

---
+++ app/components/screens/Admin/Genres/genre-edit/useGenreEdit.ts
@@ -0,0 +1,47 @@
+import { GenreServices } from '@/services/genre.service'
+import { getKeys } from '@/utils/object/getKeys'
+import { toastError } from '@/utils/toast-error'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { SubmitHandler, UseFormSetValue } from 'react-hook-form'
+import { useMutation, useQuery } from 'react-query'
+import { toastr } from 'react-redux-toastr'
+import { IGenreEditInput } from './genre-edit.interface'
+import { getAdminUrl } from '@/config/url.config'
+
+const useGenreEdit = (setValue: UseFormSetValue<IGenreEditInput>) => {
+	const { push } = useRouter()
+	const searchParams = useSearchParams()
+
+	const genreId = String(searchParams?.get('id'))
+
+	const { isLoading } = useQuery(['genre edit', genreId], () => GenreServices.getById(genreId), {
+		onSuccess({ data }) {
+			getKeys(data).forEach((key) => {
+				setValue(key, data[key])
+			})
+		},
+		onError(error) {
+			toastError(error, 'Get genre')
+		},
+		enabled: !!genreId
+	})
+
+	const { mutateAsync: updateAsync } = useMutation('update genre', (data: IGenreEditInput) => GenreServices.update(genreId, data), {
+		onSuccess() {
+			toastr.success('Update genre', 'update was success')
+			push(getAdminUrl('genres'))
+		},
+		onError(error) {
+			toastError(error, 'Update genre')
+		},
+	})
+
+	const onSubmit: SubmitHandler<IGenreEditInput> = async (data: IGenreEditInput) => updateAsync(data)
+
+	return {
+		isLoading,
+		onSubmit
+	}
+}
+
+export default useGenreEdit

938dd2938881e0a4698f6de8b2de0d5ba4da1dae
DaniilRai13 <testnet02@mail.ru>
2024-11-14 17:01:40 +0300

    feat: create methods to create, update and get movie by id for edit page

--- app/services/movie.service.ts
+++ app/services/movie.service.ts
@@ -1,3 +1,4 @@
+import { IMovieEditInput } from '@/components/screens/Admin/Movies/movie-edit/movie-edit.interface'
 import { getMoviesUrl } from '@/config/api.config'
 import { IMovie } from '@/shared/types/movie.types'
 import { axiosClassic } from 'api/interceptors'
@@ -17,6 +18,15 @@ export const MovieService = {
 		const { data: movies } = await axiosClassic.get<IMovie[]>(getMoviesUrl('/most-popular'))
 
 		return movies;
+	},
+	async getById(_id: string) {
+		return axios.get<IMovieEditInput>(getMoviesUrl(`/${_id}`))
+	},
+	async update(_id: string, data: IMovieEditInput) {
+		return axios.put<string>(getMoviesUrl(`/${_id}`), data)
+	},
+	async create() {
+		return axios.post<string>(getMoviesUrl('/'))
 	},
 	async delete(_id: string) {
 		return axios.delete<string>(getMoviesUrl(`/${_id}`))

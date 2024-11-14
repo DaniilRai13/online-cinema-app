5fd00e6a523fc2bc07073ca97aaee79cdaf92295
DaniilRai13 <testnet02@mail.ru>
2024-11-14 16:59:30 +0300

    feat: display genres with filled in fields only

--- app/components/layout/Navigation/MenuContainer/genres/usePopularGenres.ts
+++ app/components/layout/Navigation/MenuContainer/genres/usePopularGenres.ts
@@ -1,13 +1,13 @@
 import { getGenreUrl } from '@/config/url.config'
-import { GenreServices } from '@/services/genre.service'
+import { GenreService } from '@/services/genre.service'
 import { useQuery } from 'react-query'
 import { IMenuItem } from '../Menu/Menu.interface'
 
 export const usePopularGenres = () => {
-	const queryData = useQuery('popular genre menu', () => GenreServices.getAll(), {
+	const queryData = useQuery('popular genre menu', () => GenreService.getAll(), {
 		select: ({ data }) =>
 			data
-				.map(genre => ({
+				.filter(genre => genre.icon).map(genre => ({
 					icon: genre.icon,
 					link: getGenreUrl(genre.slug),
 					title: genre.name

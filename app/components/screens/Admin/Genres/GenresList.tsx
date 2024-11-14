92c5ca866334e30de35969e62f5430ea6a1c6d1e
DaniilRai13 <testnet02@mail.ru>
2024-11-14 17:15:13 +0300

    feat: add function to create genre

--- app/components/screens/Admin/Genres/GenresList.tsx
+++ app/components/screens/Admin/Genres/GenresList.tsx
@@ -8,12 +8,16 @@ import { FC } from 'react'
 import { useGenres } from './useGenres'
 
 const GenresList: FC = () => {
-	const { isLoading, handleSearch, searchTerm, data, deleteAsync } = useGenres()
+	const { isLoading, handleSearch, searchTerm, data, deleteAsync, createAsync } = useGenres()
 	return (
 		<Meta title='Users'>
 			<AdminNavigation />
 			<Heading title='Genres' />
-			<AdminHeader handleSearch={handleSearch} searchTerm={searchTerm} />
+			<AdminHeader
+				handleSearch={handleSearch}
+				searchTerm={searchTerm}
+				onClick={createAsync}
+			/>
 			<AdminTable
 				isLoading={isLoading}
 				removeHandler={deleteAsync}

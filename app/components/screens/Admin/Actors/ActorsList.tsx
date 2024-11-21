da706c1d63ef352172c54419adb579eabceea268
DaniilRai13 <testnet02@mail.ru>
2024-11-22 00:09:16 +0300

    feat: add handler function to create a new actor

--- app/components/screens/Admin/Actors/ActorsList.tsx
+++ app/components/screens/Admin/Actors/ActorsList.tsx
@@ -5,15 +5,15 @@ import Heading from '@/ui/heading/Heading'
 import Meta from '@/utils/meta/Meta'
 
 import { FC } from 'react'
-import { useActors} from './useActors'
+import { useActors } from './useActors'
 
 const ActorsList: FC = () => {
-	const { isLoading, handleSearch, searchTerm, data, deleteAsync } = useActors()
+	const { isLoading, handleSearch, searchTerm, data, deleteAsync, createActor } = useActors()
 	return (
 		<Meta title='Users'>
 			<AdminNavigation />
 			<Heading title='Users' />
-			<AdminHeader handleSearch={handleSearch} searchTerm={searchTerm} />
+			<AdminHeader handleSearch={handleSearch} searchTerm={searchTerm} onClick={createActor} />
 			<AdminTable isLoading={isLoading} removeHandler={deleteAsync} headerItems={['Name', 'Count Movies']} tableItems={data || []} />
 		</Meta>
 	)

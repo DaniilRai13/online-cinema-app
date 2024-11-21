0f7ee5bf070f88ea0e45a6ca78510fd3f063d76b
DaniilRai13 <testnet02@mail.ru>
2024-11-22 00:30:58 +0300

    feat: add mutation for creating movie with error and success handling

--- app/components/screens/Admin/Movies/useMovies.ts
+++ app/components/screens/Admin/Movies/useMovies.ts
@@ -4,6 +4,7 @@ import { MovieService } from '@/services/movie.service'
 import { ITableItem } from '@/ui/admin-table/AdminTable/adminTable.interface'
 import { getGenresList } from '@/utils/movie/getGenresListEach'
 import { toastError } from '@/utils/toast-error'
+import { useRouter } from 'next/navigation'
 import { ChangeEvent, useMemo, useState } from 'react'
 import { useMutation, useQuery } from 'react-query'
 import { toastr } from 'react-redux-toastr'
@@ -11,6 +12,7 @@ import { toastr } from 'react-redux-toastr'
 export const useMovies = () => {
 	const [searchTerm, setSearchTerm] = useState('')
 	const debounceSearch = useDebounce(searchTerm, 500)
+	const {push} = useRouter()
 
 	const queryData = useQuery(['movies list', debounceSearch], () => MovieService.getMoviesBySearchTerm(debounceSearch), {
 		select: ({ data }) => data.map((movie): ITableItem => ({
@@ -33,6 +35,16 @@ export const useMovies = () => {
 		}
 	})
 
+	const { mutateAsync: createMovie } = useMutation('create movie', () => MovieService.create(), {
+		onError: (error) => {
+			toastError(error, 'Create movie')
+		},
+		onSuccess: ({data: _id}) => {
+			toastr.success('Create movie', 'create was successful')
+			push(getAdminUrl(`movie/edit/${_id}`))
+		}
+	})
+
 	const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
 		setSearchTerm(e.target.value)
 	}
@@ -41,7 +53,8 @@ export const useMovies = () => {
 		handleSearch,
 		...queryData,
 		searchTerm,
-		deleteAsync
-	}), [queryData, searchTerm, deleteAsync]
+		deleteAsync,
+		createMovie
+	}), [queryData, searchTerm, deleteAsync,createMovie]
 	)
 }

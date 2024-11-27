f78734bcd95b700b8da923914789218d89c7936d
DaniilRai13 <testnet02@mail.ru>
2024-11-27 16:35:17 +0300

    feat: implement custom useSlider hook for managing slider state and navigation

---
+++ app/components/ui/slider/useSlider.ts
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+export const useSlider = (length: number) => {
+	const [currentIndex, setCurrentIndex] = useState<number>(0)
+	const [slideIn, setSlideIn] = useState<boolean>(true)
+
+	const isExistNext = currentIndex + 1 < length
+	const isExistPrev = currentIndex > 0
+
+	const handleArrowClick = (direction: 'next' | 'rev') => {
+		const newIndex = direction === 'next' ? currentIndex + 1 : currentIndex - 1
+		setSlideIn(false)
+
+		setTimeout(() => {
+			setCurrentIndex(newIndex)
+			setSlideIn(true)
+		}, 300)
+	}
+
+	return {
+		slideIn,
+		index: currentIndex,
+		isNext: isExistNext,
+		isPrev: isExistPrev,
+		handleClick: handleArrowClick
+	}
+}

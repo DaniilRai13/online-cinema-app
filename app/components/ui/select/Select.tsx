67718d1c0ea4bd6fde9848ffe8d8d516de9c79f1
DaniilRai13 <testnet02@mail.ru>
2024-11-27 16:08:06 +0300

    feat: create Select component used react-select

---
+++ app/components/ui/select/Select.tsx
@@ -0,0 +1,54 @@
+import { FC } from 'react'
+import ReactSelect, { OnChangeValue } from 'react-select'
+import makeAnimated from 'react-select/animated'
+import formStyles from '../form-elements/form.module.scss'
+import { IOption, ISelect } from './select.interface'
+import styles from './Select.module.scss'
+
+const animatedComponents = makeAnimated()
+
+const Select: FC<ISelect> = ({
+	placeholder,
+	error,
+	isMulti,
+	options,
+	field,
+	isLoading
+}) => {
+	debugger
+	const onChange = (newValue: OnChangeValue<IOption, boolean>) => {
+		field.onChange(isMulti
+			? (newValue as IOption[]).map((item: IOption) => item.value)
+			: (newValue as IOption).value
+		)
+	}
+	const getValue = () => {
+		if (field.value) {
+			return isMulti
+				? options.filter(option => field.value.indexOf(option.value) >= 0)
+				: options.find(option => option.value === field.value)
+		} else {
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			return isMulti ? [] : ("" as any)
+		}
+	}
+	return (
+		<div className={styles.selectContainer}>
+			<label>
+				<span>{placeholder}</span>
+				<ReactSelect
+					classNamePrefix='custom-select'
+					options={options}
+					value={getValue()}
+					isMulti={isMulti}
+					onChange={onChange}
+					components={animatedComponents}
+					isLoading={isLoading}
+				/>
+				{error && <div className={formStyles.error}>{error.message}</div>}
+			</label>
+		</div>
+	)
+}
+
+export default Select

9457e00f0ef7d623142d388e02cf1ae39265c040
DaniilRai13 <testnet02@mail.ru>
2024-11-27 16:07:00 +0300

    fix: add description value in data

--- app/services/movie.service.ts
+++ app/services/movie.service.ts
@@ -1,11 +1,10 @@
 import { IMovieEditInput } from '@/components/screens/Admin/Movies/movie-edit/movie-edit.interface'
 import { getMoviesUrl } from '@/config/api.config'
 import { IMovie } from '@/shared/types/movie.types'
-import { axiosClassic } from 'api/interceptors'
-import  axios  from 'api/interceptors'
+import axios, { axiosClassic } from 'api/interceptors'
 
 export const MovieService = {
-	async getMoviesBySearchTerm (searchTerm?: string) {
+	async getMoviesBySearchTerm(searchTerm?: string) {
 		return axiosClassic.get<IMovie[]>(getMoviesUrl(''), {
 			params: searchTerm
 				? {
@@ -23,7 +22,7 @@ export const MovieService = {
 		return axios.get<IMovieEditInput>(getMoviesUrl(`/${_id}`))
 	},
 	async update(_id: string, data: IMovieEditInput) {
-		return axios.put<string>(getMoviesUrl(`/${_id}`), data)
+		return axios.put<string>(getMoviesUrl(`/${_id}`), { ...data, description: '' })
 	},
 	async create() {
 		return axios.post<string>(getMoviesUrl('/'))

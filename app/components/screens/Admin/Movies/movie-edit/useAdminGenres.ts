2de24d93fe653e22d9eb1069720f976abebbe8eb
DaniilRai13 <testnet02@mail.ru>
2024-11-27 16:11:32 +0300

    feat: get some data about genres for Select component

---
+++ app/components/screens/Admin/Movies/movie-edit/useAdminGenres.ts
@@ -0,0 +1,19 @@
+import { IOption } from '@/components/ui/select/select.interface'
+import { GenreService } from '@/services/genre.service'
+import { toastError } from '@/utils/toast-error'
+import { useQuery } from 'react-query'
+
+export const useAdminGenres = () => {
+	const queryData = useQuery(['genre select'], () => GenreService.getAll(), {
+		select: ({ data }) => data.map(
+			(genre): IOption => ({
+				label: genre.name,
+				value: genre._id
+			})
+		),
+		onError(error) {
+			toastError(error, 'Select Genre')
+		}
+	})
+	return queryData
+}

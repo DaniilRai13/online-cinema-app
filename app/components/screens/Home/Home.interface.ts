041c2eb0a3ab705fb70bd38ac0d202e825c0f3cf
DaniilRai13 <testnet02@mail.ru>
2024-11-28 23:39:19 +0300

    feat: add new prop to interface

--- app/components/screens/Home/Home.interface.ts
+++ app/components/screens/Home/Home.interface.ts
@@ -1,7 +1,13 @@
+import { IGalleryItem } from '@/components/ui/gallery/gallery.interface'
+import { ISlide } from '@/components/ui/slider/slider.interface'
 import { ReactNode } from 'react'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface IHome { }
+export interface IHome {
+	slides: ISlide[]
+	actors: IGalleryItem[]
+	trendingMovies: IGalleryItem[]
+}
 export interface ILayout {
 	children?: ReactNode
 }

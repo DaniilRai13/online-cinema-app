51bf43183b499efec0d216bf6767daf40a828a97
DaniilRai13 <testnet02@mail.ru>
2024-11-28 23:40:27 +0300

    feat: add actor gallery and trending movies gallery

--- app/components/screens/Home/Home.tsx
+++ app/components/screens/Home/Home.tsx
@@ -1,15 +1,28 @@
-import Heading from '@/components/ui/heading/Heading'
+import Gallery from '@/ui/gallery/Gallery'
+import Heading from '@/ui/heading/Heading'
+import SubHeading from '@/ui/heading/SubHeading'
+import Slider from '@/ui/slider/Slider'
 import Meta from '@/utils/meta/Meta'
-import { NextPage } from 'next'
+import { FC } from 'react'
 import { IHome } from './Home.interface'
 
-const Home: NextPage<IHome> = () => {
+const Home: FC<IHome> = ({ slides, actors, trendingMovies }) => {
 	return (
 		<Meta
 			title="Watch movies online"
 			description="Watch MovieApp movies and TV shows online or stream right to your browser."
 		>
 			<Heading title='Watch movies online' className='text-gray-300 mb-8 text-xl' />
+			{slides.length && <Slider slides={slides} buttonTitle='Watch Movie' />}
+
+			<div className='my-8'>
+				<SubHeading title='Trending movies' />
+				{trendingMovies.length && < Gallery items={trendingMovies} />}
+			</div>
+			<div>
+				<SubHeading title='Best Actors' />
+				{actors.length && <Gallery items={actors} />}
+			</div>
 		</Meta>
 	)
 }
